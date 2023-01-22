d0094223b0d098b081a76ad799142fdee7397a94
kaw101010 <krishwadhwani0601@gmail.com>
2023-01-21 20:21:23 -0500

    Add level 1

--- redirect_screen.py
+++ redirect_screen.py
@@ -21,25 +21,3 @@ def show_new_screen(screen, image):
                 if event.key == pygame.K_q:
                     running = False
     return False
-
-
-# def show_new_screen(screen, image):
-#     # Load the image
-#     image = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-#
-#     # Scale the image to the size of the screen
-#     image = pygame.transform.scale(image,
-#                                    (1920, 1080))
-#
-#     # Draw the image on the screen
-#     screen.blit(image, (0, 0))
-#     pygame.display.update()
-#     running = True
-#     while running:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 running = False
-#             elif event.type == pygame.KEYDOWN:
-#                 if event.key == pygame.K_q:
-#                     running = False
-#     return False
